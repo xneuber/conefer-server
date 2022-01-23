b85e75d2f57aa5064e82f2f58c1f3d8bfb980d2a
whiplaxx <whiplaxx@gmail.com>
2022-01-23 16:29:58 -0300

    Cadastro produtos

--- src/main/java/br/edu/ifg/manutencao/coneferserver/entity/Unidade.java
+++ src/main/java/br/edu/ifg/manutencao/coneferserver/entity/Unidade.java
@@ -1,5 +1,6 @@
 package br.edu.ifg.manutencao.coneferserver.entity;
 
+import br.edu.ifg.manutencao.coneferserver.entity.enums.EnumEstado;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -7,6 +8,8 @@ import lombok.Setter;
 import org.hibernate.annotations.DynamicUpdate;
 
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -15,13 +18,14 @@ import javax.persistence.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
-@SequenceGenerator(name = "default_gen", sequenceName = "grupo_id_seq", allocationSize = 1)
-public class Grupo extends Entidade {
+@SequenceGenerator(name = "default_gen", sequenceName = "unidade_id_seq", allocationSize = 1)
+public class Unidade extends Entidade {
+
+    private String descricao;
 
     @Column(nullable = false)
-    private String nomeGrupo;
+    private String unidade;
 
     @Column(nullable = false)
-    private Boolean geraComissao;
-
+    private Boolean isInt;
 }
