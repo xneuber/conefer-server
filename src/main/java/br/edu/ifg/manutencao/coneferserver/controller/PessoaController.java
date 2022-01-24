6da016b46c62a6982536b00983e236d0446747de
xneuber <xneuber@gmail.com>
2022-01-24 08:45:02 -0300

    Revert "corrigindo entidades"
    This reverts commit ddd5decc1ce5fcfe2daa7996a56e07ea0dc93751.

--- src/main/java/br/edu/ifg/manutencao/coneferserver/controller/PessoaController.java
+++ src/main/java/br/edu/ifg/manutencao/coneferserver/controller/PessoaController.java
@@ -1,9 +1,11 @@
 package br.edu.ifg.manutencao.coneferserver.controller;
 
-import br.edu.ifg.manutencao.coneferserver.entity.Fornecedor;
+import br.edu.ifg.manutencao.coneferserver.entity.Pessoa;
 import br.edu.ifg.manutencao.coneferserver.exception.ConeferException;
 import br.edu.ifg.manutencao.coneferserver.exception.InfoMessage;
-import br.edu.ifg.manutencao.coneferserver.service.FornecedorService;
+import br.edu.ifg.manutencao.coneferserver.model.request.PessoaRequest;
+import br.edu.ifg.manutencao.coneferserver.model.response.PessoaResponse;
+import br.edu.ifg.manutencao.coneferserver.service.PessoaService;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
@@ -15,46 +17,46 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/fornecedors")
-public class FornecedorController extends GenericoController<Fornecedor> {
+@RequestMapping("/pessoas")
+public class PessoaController extends GenericoController<Pessoa> {
 
-    private final FornecedorService grupoDocumentoService;
+    private final PessoaService grupoDocumentoService;
 
     @Autowired
-    public FornecedorController(FornecedorService grupoDocumentoService) {
+    public PessoaController(PessoaService grupoDocumentoService) {
         this.grupoDocumentoService = grupoDocumentoService;
     }
 
     @GetMapping(value = "/lista-page", produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ApiOperation(value = "Realiza a listagem das fornecedors")
+    @ApiOperation(value = "Realiza a listagem das pessoas")
     public ResponseEntity<?> listaPaginadoNew(@RequestParam(value = "nome", required = false, defaultValue = "") String nome,
                                               Pageable pageable) {
         return ResponseEntity.ok().body(this.getService().listarTodosPage(nome, pageable));
     }
 
     @GetMapping(value = "/{uuid}", produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ApiOperation(value = "Realiza a consulta de uma fornecedor")
-    public ResponseEntity<Fornecedor> consultar(@PathVariable String uuid){
-        Fornecedor fornecedor = this.getService().consultarPeloUuid(uuid);
-        return ResponseEntity.status(HttpStatus.OK).body(fornecedor);
+    @ApiOperation(value = "Realiza a consulta de uma pessoa")
+    public ResponseEntity<PessoaResponse> consultar(@PathVariable String uuid){
+        PessoaResponse grupoDocumentoResponse = this.getService().consultarPeloUuid(uuid);
+        return ResponseEntity.status(HttpStatus.OK).body(grupoDocumentoResponse);
     }
 
     @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ApiOperation(value = "Realiza a criação de uma fornecedor")
-    public ResponseEntity<FornecedorResponse> salvar(@Valid @RequestBody FornecedorRequest fornecedorRequest){
-        FornecedorResponse entidadeModeloCriada = this.getService().salvar(fornecedorRequest);
+    @ApiOperation(value = "Realiza a criação de uma pessoa")
+    public ResponseEntity<PessoaResponse> salvar(@Valid @RequestBody PessoaRequest pessoaRequest){
+        PessoaResponse entidadeModeloCriada = this.getService().salvar(pessoaRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(entidadeModeloCriada);
     }
 
     @PutMapping(consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ApiOperation(value = "Realiza a atualização de uma fornecedor")
-    public ResponseEntity<FornecedorResponse> atualizar(@Valid @RequestBody FornecedorRequest fornecedorRequest) throws ConeferException {
-        FornecedorResponse modeloDocumentoCriado = this.getService().atualizar(fornecedorRequest);
+    @ApiOperation(value = "Realiza a atualização de uma pessoa")
+    public ResponseEntity<PessoaResponse> atualizar(@Valid @RequestBody PessoaRequest pessoaRequest) throws ConeferException {
+        PessoaResponse modeloDocumentoCriado = this.getService().atualizar(pessoaRequest);
         return ResponseEntity.status(HttpStatus.OK).body(modeloDocumentoCriado);
     }
 
     @DeleteMapping(value = "/{uuid}", consumes = {MediaType.APPLICATION_JSON_VALUE}, produces = {MediaType.APPLICATION_JSON_VALUE})
-    @ApiOperation(value = "Realiza a exclusão de uma fornecedor")
+    @ApiOperation(value = "Realiza a exclusão de uma pessoa")
     public ResponseEntity<InfoMessage> excluir(@PathVariable String uuid) throws ConeferException {
         this.getService().excluirPeloId(uuid);
         InfoMessage excluidoComSucesso = InfoMessage.builder().status(HttpStatus.OK.value()).mensagem("Excluído com sucesso").build();
@@ -62,7 +64,7 @@ public class FornecedorController extends GenericoController<Fornecedor> {
     }
 
     @Override
-    public FornecedorService getService() {
+    public PessoaService getService() {
         return this.grupoDocumentoService;
     }
 }
